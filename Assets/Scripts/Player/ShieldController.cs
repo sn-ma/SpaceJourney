73c85751ed7898bebc6551366f4fdc602ae01500
Nikolay-Maryan Sokolovsky <n.m.sokolovsky@gmail.com>
2022-07-01 01:05:31 +0300

    Switch shield control to animation; add light to it

--- Assets/Scripts/Player/ShieldController.cs
+++ Assets/Scripts/Player/ShieldController.cs
@@ -1,58 +1,18 @@
 using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class ShieldController : MonoBehaviour
 {
-    public float fadeInSpeed = 1f;
-    public float delayTimeSec = 0.5f;
-    public float fadeOutSpeed = 0.1f;
-
-    private SpriteRenderer spriteRenderer;
-    private Color initialColor;
-
-    private Coroutine coroutine = null;
+    private Animator animator;
 
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        initialColor = spriteRenderer.material.color;
-
-        SetAlpha(0f);
+        animator = GetComponent<Animator>();
     }
 
     public void StartFadeAnimation()
     {
-        if (coroutine != null)
-        {
-            StopCoroutine(coroutine);
-        }
-        coroutine = StartCoroutine(FadeAnimationObject());
-    }
-
-    public IEnumerator FadeAnimationObject()
-    {
-        while (spriteRenderer.material.color.a < 1f)
-        {
-            SetAlpha(GetAlpha() + fadeInSpeed * Time.deltaTime);
-            yield return null;
-        }
-
-        yield return new WaitForSeconds(delayTimeSec);
-
-        while (spriteRenderer.material.color.a > 0f)
-        {
-            SetAlpha(GetAlpha() - fadeOutSpeed * Time.deltaTime);
-            yield return null;
-        }
-    }
-
-    private float GetAlpha()
-    {
-        return spriteRenderer.material.color.a;
-    }
-
-    private void SetAlpha(float alpha)
-    {
-        spriteRenderer.material.color = new Color(initialColor.r, initialColor.g, initialColor.b, alpha);
+        animator.SetTrigger("Hit");
     }
 }
